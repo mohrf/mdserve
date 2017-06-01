9b860777c19131072c3d10570f3d1f351b0a0bcd
mohrf <fernandomohr@gmail.com>
2017-05-31 22:56:02 -0300

    Memoize watchers and configure style with .editorconfig

--- bin/mdserve.js
+++ bin/mdserve.js
@@ -5,59 +5,56 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const fs = require('fs');
 const chokidar = require('chokidar');
-const md = require('markdown-it')(require('../lib/highlight-code')());
 const path = require('path');
+// own
+const md = require('markdown-it')(require('../lib/highlight-code')());
 
 // set templating engine
-app.set('view engine', 'pug'); 
-
-// file watchers
-const watchers = []; 
+app.set('view engine', 'pug');
 
 // emit socket message when file content change
 function watchFile(fileName){
-    if (watchers[fileName])
-	return;
+  watchFile.watchers = watchFile.watchers || [];
 
-    watchers[fileName] = chokidar.watch(fileName); 
-    watchers[fileName].on('change', (path) => {
-	openFile(fileName, (err, data) => { 
+  if (watchFile.watchers[fileName])
+	  return;
+
+  watchFile.watchers[fileName] = chokidar.watch(fileName);
+  watchFile.watchers[fileName].on('change', (path) => {
+	  openFile(fileName, (err, data) => {
 	    if (!err)
-		io.emit(fileName, md.render(data));
-	}); 
-    });
+		    io.emit(fileName, md.render(data));
+	  });
+  });
 }
 
-
-// Open file from file system 
+// open file from file system
 function openFile(fileName, callback){
-    fs.readFile(fileName, 'utf8', callback); 
-} 
+  fs.readFile(fileName, 'utf8', callback);
+}
 
 server.listen(2212);
 
 io.on('connection', () => {
-    console.log('new client connected');
-}); 
+  console.log('new client connected');
+});
 
 // share hightlight.js files
 const dir = path.join(__dirname, '..', 'node_modules');
 app.use('/static', express.static(dir)) ;
 
-// Route that opens markdown file
-app.get('/:file', (req, res) => { 
-    let fileName = __dirname + '/' + req.params.file;
-    openFile(fileName, (err, data) => {
-	if (err){
+// route that opens markdown file
+app.get('/:file', (req, res) => {
+  let fileName = path.join(__dirname, req.params.file);
+  openFile(fileName, (err, data) => {
+	  if (err){
 	    res.status(404).send('Not found');
-	} else {
+	  } else {
 	    res.render('index', {
-		fileName: fileName,
-		content: md.render(data)
+		    fileName: fileName,
+		    content: md.render(data)
 	    });
 	    watchFile(fileName);
-	}
-    });  
+	  }
+  });
 });
-
-
